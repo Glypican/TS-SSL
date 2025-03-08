526b190cf7b1e4c75a47a48a912658135d74da84
Glypican <hghani@carisls.com>
2025-03-07 16:03:41 -0800

    Update __init__.py

--- tumor_highlighter/data/__init__.py
+++ tumor_highlighter/data/__init__.py
@@ -1,6 +1,7 @@
 """Data handling components for Tumor Highlighter."""
 
-from tumor_highlighter.data.wsi_dataset import WSIDataset, PatchDataset
-from tumor_highlighter.data.tissue_mask import HSVTissueMaskGenerator
+from tumor_highlighter.data.wsi_dataset import WSIDataset
+from tumor_highlighter.data.patch_dataset import PatchDataset
+from tumor_highlighter.data.mask_generator import HSVTissueMaskGenerator
 
 __all__ = ["WSIDataset", "PatchDataset", "HSVTissueMaskGenerator"]
